af66f6d9d40b824667845a1fbf54a3f73ce6bad8
Arun Shriram <ashriram@calpoly.edu>
2020-02-25 10:19:31 -0800

    Added algorithm to determine fundamental frequencies from each window

--- test.py
+++ test.py
@@ -20,19 +20,36 @@ def freqToPitch(freq):
     n = h % 12
     return name[n] #+ str(octave)
 
-# Takes a list of pitch frequencies from FFT analysis, and returns a list of the fundamental frequencies found.
+# Takes a ascended-sorted list of pitch frequencies from FFT analysis, and returns a list of the fundamental frequencies found.
 def getFundamentalFrequencies(pitchFrequencies):
     if len(pitchFrequencies) < 0:
         return []
     if len(pitchFrequencies) == 1:
         return pitchFrequencies 
     funFreqs = []
+    checkedFrequencies = []
+    index = 0
     for freq in pitchFrequencies:
-        if funFreqs == []:
+        numberOfOvertones = 0
+        for hi_freq_index in range(index + 1, len(pitchFrequencies)): # checking the right part of the list
+            hi_freq = pitchFrequencies[hi_freq_index]
+            if hi_freq not in checkedFrequencies and checkIfOvertone(freq, hi_freq): # this means that the hi freq pitch is an overtone of freq
+                numberOfOvertones += 1
+                checkedFrequencies.append(hi_freq) # We don't want to see this pitch again
+        if numberOfOvertones > 0:
             funFreqs.append(freq)
-        else:
-            for i in range(1, 9):
-                pass
+        index += 1
+    return funFreqs
+
+# Takes two pitch frequencies, and determines if the second is an overtone (a multiple of 2^n of the first)
+# Returns True if pitchB is an overtone of pitchA
+def checkIfOvertone(pitchA, pitchB):
+    epsilon = 0.03*pitchB 
+    for power in range(1, 8): # pitches won't get higher than 2^9 of some value
+        theoreticalOvertoneValue = pow(2, power)*pitchA
+        if theoreticalOvertoneValue <= pitchB + epsilon and theoreticalOvertoneValue >= pitchB-epsilon:
+            return True
+    return False
             
 def showPlotForSample(rate, samples):
         # plt.figure(1)
@@ -52,13 +69,13 @@ def showPlotForSample(rate, samples):
         fourier_to_plot = abs(fourier[0:len(fourier)//2])
         w = w[0:len(fourier)//2]
 
-        indexesOfPeaks = peakutils.indexes(fourier_to_plot, thres=0.07)
+        indexesOfPeaks = peakutils.indexes(fourier_to_plot, thres=0.04)
         peakFrequencies = [w[u] for u in indexesOfPeaks]
         
-        print(getFundamentalFrequencies(peakFrequencies))
         xxx = peakutils.interpolate(w, fourier_to_plot, ind=indexesOfPeaks)
         pitchPeaks = [freqToPitch(freq) for freq in peakFrequencies]
         print(peakFrequencies)
+        print("Found these fundamental frequencies: %s" % str(getFundamentalFrequencies(peakFrequencies)))
         # print("XXX: %s" % str([freqToPitch(freq) for freq in xxx]))
         # print("Peak Frequencies: %s" % str(pitchPeaks))
         # print()
@@ -67,8 +84,7 @@ def showPlotForSample(rate, samples):
         plt.ylabel('amplitude')
         plt.plot(w[:int(len(w)/10)], fourier_to_plot[:int(len(fourier_to_plot)/10)])
         plt.show()
-        return pitchPeaks
-
+        return getFundamentalFrequencies(peakFrequencies)
 def processFile(filename):
     
     downsample = 1
@@ -91,14 +107,15 @@ def processFile(filename):
     while True:
         samples, read = s()
         frequentPitches = showPlotForSample(44100, samples)
-        for pitch in frequentPitches:
+        frequentNotes = [freqToPitch(freq) for freq in frequentPitches]
+        for pitch in frequentNotes:
             if pitch not in mostFrequentPitches:
                 mostFrequentPitches[pitch] = 0
             else:
                 mostFrequentPitches[pitch] += 1
         total_frames += read
-        print(read)
-        print(hop_s)
+        # print(read)
+        # print(hop_s)
         if read < hop_s: break
         count += 1
 
@@ -107,53 +124,63 @@ def processFile(filename):
     # print(mostFrequentPitches)
     return sorted(mostFrequentPitches.items(), reverse=True, key = lambda kv: (kv[1], kv[0]))
 
-def main():
+def testRagam():
     arohanam = [Note("S"), Note("M", 1), Note("P"), Note("S")]
     avarohanam = [Note("S"), Note("P"), Note("M", 1), Note("S")]
     ragdb = RagamDB("full-ragam-list.txt")
     print(ragdb.searchByScales(arohanam, avarohanam))
-    # print()
-    # print("Beginning sruthi analysis...")
-    # print()
-    # successTracker = {}
-    # count = 1
-    # file = "small_test_3.mp3"
-    # pitches = processFile(file)
-    # print(pitches)
     
-    # for filenam in os.listdir("TestData"):
-    #     if(not (filenam.endswith("mp3") or filenam.endswith("m4a") or filenam.endswith("wav"))):
-    #         continue
-    #     file_to_process = "TestData/" + filenam
-        
-    #     file = filenam.split('.')[0]
-    #     print("%d. Verifying %s......" % (count, file), end="")
-    #     pitches = processFile(file_to_process)
-    #     print(pitches)
-    #     if(pitches[0][0] == file.split("_")[0]):
-    #         print("Success")
-    #         successTracker[filenam] = "Success"
-    #     else:
-    #         print("Failure! Found %s instead." % pitches[0][0])
-    #         successTracker[filenam] = "...Failure"
-            
-    #     print('\n')
-    #     count += 1
-    # # audioAnalyzer.plotPitches()
+def testSmallFile():
+    file = "small_test_5.mp3"
+    pitches = processFile(file)
+    print(pitches)
 
-    # # Print brief summary report
-    # count = 1
-    # print()
-    # successes = 0
-    # total = 0
-    # for item in successTracker:
-    #     print("%d. %s\t.....%s" % (count, item, successTracker[item]))
-    #     if successTracker[item] == "Success":
-    #         successes += 1
-    #     total += 1
-    #     count += 1
-    # print()
-    # print("Success rate: %s%%" % ('{0:.2f}'.format(100*(successes/float(total)))))
+def testAllData():
+    successTracker = {}
+    count = 1
+    for filenam in os.listdir("TestData"):
+        if(not (filenam.endswith("mp3") or filenam.endswith("m4a") or filenam.endswith("wav"))):
+            continue
+        file_to_process = "TestData/" + filenam
+        
+        file = filenam.split('.')[0]
+        print("%d. Verifying %s......" % (count, file), end="")
+        pitches = processFile(file_to_process)
+        print(pitches)
+        if(pitches[0][0] == file.split("_")[0]):
+            print("Success")
+            successTracker[filenam] = "Success"
+        else:
+            print("Failure! Found %s instead." % pitches[0][0])
+            successTracker[filenam] = "...Failure"
+            
+        print('\n')
+        count += 1
+    # audioAnalyzer.plotPitches()
+
+    # Print brief summary report
+    count = 1
+    print()
+    successes = 0
+    total = 0
+    for item in successTracker:
+        print("%d. %s\t.....%s" % (count, item, successTracker[item]))
+        if successTracker[item] == "Success":
+            successes += 1
+        total += 1
+        count += 1
+    print()
+    print("Success rate: %s%%" % ('{0:.2f}'.format(100*(successes/float(total)))))
+    
+def main():
+    # testRagam()
+    print()
+    print("Beginning sruthi analysis...")
+    print()
+    
+    testSmallFile()
+    
+    # testAllData()
     
 if __name__ == '__main__':
     main()
